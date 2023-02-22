0d7b4a7c6853c4fb72158c366e15d9b7c1f4e3e3
ZubairARooghwall <99389773+ZubairARooghwall@users.noreply.github.com>
2023-02-22 10:37:07 +0430

    Add event listener and set conditions

--- script.js
+++ script.js
@@ -1,4 +1,90 @@
+// important variables
+let display = " ";
+let prevDisplay = " ";
+let operator = " ";
+let displayNumber = document.getElementById("now");
+let displayPrevNum = document.getElementById("previous");
+
+
+
+// function that populated the display area
+// add an event listener
+let buttonsNumber = document.querySelectorAll(".btn");
+buttonsNumber.forEach((item) =>{
+    
+    item.addEventListener('click', (e) =>{
+        console.log(e);
+        if(item.textContent != "+" && item.textContent != "-" && item.textContent != "*" && item.textContent != "/" && item.textContent != "Clear" && item.textContent != "="){
+            display += item.textContent;
+            console.log(`Display check: ${display}`);
+            displayNumber.textContent = display;
+            console.log(`Output check: ${displayNumber.textContent}`);
+        }else{
+            let prevOperator;
+            operator = item.textContent;
+            console.log(`Operator check: ${operator}`);
+            // displayPrevNum.textContent = `${prevDisplay} ${operator}`;
+            if(item.textContent == "+" || item.textContent == "-" || item.textContent == "*"  || item.textContent == "/" ){
+                
+
+
+                if(prevDisplay == " "){
+                    
+                    prevDisplay = display;
+                    display = " ";
+                    displayNumber.textContent = " ";
+                }
+                if(prevDisplay != " "){
+                    prevDisplay = operate(Number(prevDisplay), Number(display), operator);
+                    // displayPrevNum.textContent = `${prevDisplay} ${operator}`;
+                    display = " ";
+                    displayNumber.textContent = " ";
+
+
+                }
+                
+
+
+
+            }else if(item.textContent == "Clear"){
+                display = " ";
+                prevDisplay = " ";
+                displayNumber.textContent = " ";
+                displayPrevNum.textContent = " ";
+            }else if(item.textContent == "="){
+                
+
+
+
+            }
+
+
+
+        }
+
+
+    });
+
+});
+
+
+
+
+
+//take in numbers and an operator
+function operate(num1, num2, operator){
+    let result;
+    if(operator == "+") result = add(num1, num2);
+    else if(operator == "-") result = subtract(num1, num2);
+    else if(operator == "*") result = multiply(num1, num2);
+    else if(operator == "/") result = divide(num1, num2);
+
+    return result;
+}
+
+
 //functions for operations
+//Don't reference them
 function add(num1, num2){
     return num1 + num2;
 }
@@ -10,15 +96,4 @@ function multiply(num1, num2){
 }
 function divide(num1, num2){
     return num1 / num2;
-}
-
-//take in numbers and an operator
-function operate(num1, num2, operator){
-    let result;
-    if(operator == "+") result = add(num1, num2);
-    else if(operator == "-") result = subtract(num1, num2);
-    else if(operator == "*") result = multiply(num1, num2);
-    else if(operator == "/") result = divide(num1, num2);
-
-    return result;
 }
